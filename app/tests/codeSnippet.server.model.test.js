dda3e0a71925a1af41c2dd0d132f6c2308f6f0ac
Medhir Bhargava <medhirbhargava@gmail.com>
2015-02-23 15:26:31 -0500

    Added codeSnippet server unit tests, removed old user stories, added new userstories

---
+++ app/tests/codeSnippet.server.model.test.js
@@ -0,0 +1,74 @@
+'use strict';
+
+//Module dependencies
+var should = require('should'),
+	mongoose = require('mongoose'),
+	User = mongoose.model('User'),
+	CodeSnippet = mongoose.model('CodeSnippet');
+
+/*
+** Globals
+*/
+
+var user, codeSnippet;
+
+/*
+** Unit tests
+*/
+
+describe('Code Snippet Model Unit Tests', function() {
+	beforeEach(function(done) {
+		user = new User({
+			firstName: 'First',
+			lastName: 'Last',
+			fullName: 'First Last',
+			email: 'test@databasesareawesome.org',
+			username: 'joeSmith',
+			password: 'awfulPassword'
+		});
+
+		user.save(function() {
+			codeSnippet = new CodeSnippet({
+				mode: 'R',
+				code: 'This is a line of code with a newline \n',
+				user: user
+			});
+
+			done();
+		});
+	});
+
+	describe('Method Save', function() {
+		it('should be able to save without problems', function(done) {
+			return codeSnippet.save(function(err) {
+				should.not.exist(err);
+				done();
+			});
+		});
+
+		it('should return an error when trying to save without code', function(done) {
+			codeSnippet.code = '';
+
+			return codeSnippet.save(function(err) {
+				should.exist(err);
+				done();
+			});
+		});
+
+		it('should return an error when trying to save without a mode', function(done) {
+			codeSnippet.mode = '';
+
+			return codeSnippet.save(function(err) {
+				should.exist(err);
+				done();
+			});
+		});
+	});
+
+	afterEach(function(done) {
+		CodeSnippet.remove().exec();
+		User.remove().exec();
+
+		done();
+	});
+});
