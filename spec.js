7e5fd820b4c3bd96d1b9a5be12f01850d8d56ae9
John Calderaio <jcalderaio@gmail.com>
2015-04-22 02:44:53 -0400

    Finalized Protractor Testing

--- spec.js
+++ spec.js
@@ -25,7 +25,6 @@ var randomUser = randomLast + "@ufl.edu";
 var randomPass = generateUUID();
 var comment = generateUUID();
 
-var codeSnippetSubmit = element(by.model('codeSnippetSubmit'));
 var query = element(by.model('query'));
 
 
@@ -43,35 +42,35 @@ describe('Getting to the homepage', function() {
 
 describe('Signing up', function() {
 
-	//it ('should be able to reject an existing username', function() {
-	//	browser.get('http://localhost:3000/#!/signup');
-	//	fname.sendKeys('TestFirst');
-	//	lname.sendKeys('TestLast');
-	//	username.sendKeys('Test@ufl.edu');
-	//	password.sendKeys('test123');
-	//	// confirmpassword.sendKeys('test123') ***
-	//	research.sendKeys('This is my research interests. I like to test.');
-    //
-	//	element(by.buttonText('Sign up')).click();
-    //
-	//	expect($('[data-ng-show="error"]').isDisplayed()).toBeTruthy();
-	//});
+	it ('should be able to reject an existing username', function() {
+		browser.get('http://localhost:3000/#!/signup');
+		fname.sendKeys('TestFirst');
+		lname.sendKeys('TestLast');
+		username.sendKeys('Test@ufl.edu');
+		password.sendKeys('test123');
+		// confirmpassword.sendKeys('test123') ***
+		research.sendKeys('This is my research interests. I like to test.');
 
-	//it ('should not sign up for a non-UFL email', function(){
-	//	browser.get('http://localhost:3000/#!/signup');
-	//	fname.sendKeys('TestFirst');
-	//	lname.sendKeys('TestLast');
-	//	username.sendKeys('Test@yahoo.edu');
-	//	password.sendKeys('test123');
-	//	// confirmpassword.sendKeys('test123') ***
-	//	research.sendKeys('This is my research interests. I like to test. ');
-    //
-	//	element(by.buttonText('Sign up')).click();
-    //
-	//	expect($('[data-ng-show="error"]').isDisplayed()).toBeTruthy();
-    //
-	//});
-    //
+		element(by.buttonText('Sign up')).click();
+
+		expect($('[data-ng-show="error"]').isDisplayed()).toBeTruthy();
+	});
+
+	it ('should not sign up for a non-UFL email', function(){
+		browser.get('http://localhost:3000/#!/signup');
+		fname.sendKeys('TestFirst');
+		lname.sendKeys('TestLast');
+		username.sendKeys('Test@yahoo.edu');
+		password.sendKeys('test123');
+		// confirmpassword.sendKeys('test123') ***
+		research.sendKeys('This is my research interests. I like to test. ');
+
+		element(by.buttonText('Sign up')).click();
+
+		expect($('[data-ng-show="error"]').isDisplayed()).toBeTruthy();
+
+	});
+
 	it ('should be able to create a new user', function() {
 
 		browser.get('http://localhost:3000/#!/signup');
@@ -93,41 +92,41 @@ describe('Signing up', function() {
 ////////Login/Out///////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 
-//describe('Logging Out',function() {
-//	it ('should be able to log out', function() {
-//		dropdown.click();
-//		signout.click();
-//		expect($('[data-ng-show="error"]').isDisplayed()).toBeFalsy();
-//	});
-//});
-//
-//describe('Logging in',function() {
-//	it ('should be able to log in', function() {
-//		//browser.get('http://localhost:3000/#!/');
-//		//username.sendKeys('kenan@ufl.edu');
-//		username.sendKeys(randomUser);
-//
-//		//password.sendKeys('dbShare2015');
-//		password.sendKeys(randomPass);
-//
-//		element(by.buttonText('Sign in')).click();
-//
-//		expect($('[data-ng-show="error"]').isDisplayed()).toBeFalsy();
-//	});
-//});
+describe('Logging Out',function() {
+	it ('should be able to log out', function() {
+		dropdown.click();
+		signout.click();
+		expect($('[data-ng-show="error"]').isDisplayed()).toBeFalsy();
+	});
+});
+
+describe('Logging in',function() {
+	it ('should be able to log in', function() {
+		//browser.get('http://localhost:3000/#!/');
+		//username.sendKeys('kenan@ufl.edu');
+		username.sendKeys(randomUser);
+
+		//password.sendKeys('dbShare2015');
+		password.sendKeys(randomPass);
+
+		element(by.buttonText('Sign in')).click();
+
+		expect($('[data-ng-show="error"]').isDisplayed()).toBeFalsy();
+	});
+});
 
 
 ////////////////////////////////////////////////////////////////
 ////////Profile/////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 
-//describe('Viewing your profile', function(){
-//	it ('should be able to view your profile', function(){
-//		browser.get('http://localhost:3000/#!/databases');
-//		element(by.id('dispName')).click();
-//		expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/#!/settings/profile');
-//	});
-//});
+describe('Viewing your profile', function(){
+	it ('should be able to view your profile', function(){
+		browser.get('http://localhost:3000/#!/databases');
+		element(by.id('dispName')).click();
+		expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/#!/settings/profile');
+	});
+});
 
 
 ////////////////////////////////////////////////////////////////
@@ -172,6 +171,8 @@ describe('Creating a new comment', function() {
 describe('Checking comment created', function() {
 
 	it ('should be able to confirm comment added', function() {
+
+		browser.sleep(3000);
 		element(by.id('commentTab')).click();
 
 		var array = element.all(by.repeater('comment in dbComments').column('comment.reviews')).map(function (elm) {
@@ -196,11 +197,9 @@ describe('Adding database to portfolio', function() {
 	});
 });
 
-describe('Adding database to portfolio', function() {
+describe('Checking database created', function() {
 	it ('should be able to see the database in portfolio', function() {
 		element(by.id('dispName')).click();
-
-		//var foo = element.all(by.repeater('portfolio in user.portfolios')).column('portfolio.name').getText();
 
 		var array = element.all(by.repeater('portfolio in user.portfolios').column('portfolio.name')).map(function (elm) {
 			return elm.getText();
@@ -210,29 +209,13 @@ describe('Adding database to portfolio', function() {
 	});
 });
 
-
-
-
-
-//
-//describe('Creating a new codeSnippet', function() {
-// 	it ('should be able to add a codeSnippet to the db', function() {
-// 		browser.get('http://localhost:3000/#!/552d85612e01ff00006eee89');
-//
-// 		query.sendKeys('gatorDB');
-//
-// 	});
-//});
-
-//describe('Deleting a user', function() {
-//	it ('should be able to delete a user', function() {
-//		browser.get('http://localhost:3000/#!/settings/edit');
-//		element(by.id('deleteMe')).click();
-//
-//
-//
-//
-//	});
-//});
+//Finally Signout
+describe('Logging Out',function() {
+	it ('should be able to log out', function() {
+		dropdown.click();
+		signout.click();
+		expect($('[data-ng-show="error"]').isDisplayed()).toBeFalsy();
+	});
+});
 
 
